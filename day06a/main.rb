f6fa165ada10b004bd5a9cdfa78a685234041751
gkemmey <gray.kemmey@gmail.com>
2023-12-06 00:44:26 -0500

    day06

---
+++ day06a/main.rb
@@ -0,0 +1,36 @@
+class Record
+  def initialize(time, distance)
+    @time, @distance = time, distance
+  end
+
+  def number_of_ways_to_best
+    0.upto(time).count { |t_held| t_held * (time - t_held) > distance }
+  end
+
+  private
+
+    attr_reader :time, :distance
+end
+
+def parse_times(line)
+  line.scan(/\d+/).map(&:to_i)
+end
+
+alias parse_distances parse_times
+
+def parse_records(input)
+  parse_times(input.lines[0]).zip(parse_distances(input.lines[1])).collect { |(t, d)| Record.new(t, d) }
+end
+
+# -------- main --------
+
+fail unless [4, 8, 9] == parse_records(DATA.read).collect(&:number_of_ways_to_best)
+
+fail unless 449550 == parse_records(File.read("./input.txt")).
+                        collect(&:number_of_ways_to_best).
+                        inject(:*).
+                        tap { |answer| puts(answer) }
+
+__END__
+Time:      7  15   30
+Distance:  9  40  200
